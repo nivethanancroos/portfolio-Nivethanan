50d6b887eca9aeea44acac57b716a60d87e4def0
Anton Rex Nivethanan Croos <nivethanancroos14@gmail.com>
2026-07-08 14:57:19 +0530

    gz Images fixed

--- frontend/src/components/Projects/GZ/VedioSectionGz.jsx
+++ frontend/src/components/Projects/GZ/VedioSectionGz.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import GzVedio from "../../../assets/images/Gz/Gzvedio.MOV";
+import GzVedio from "../../../assets/Images/Gz/Gzvedio.MOV";
 
 const VedioSectionGz = () => {
   return (
